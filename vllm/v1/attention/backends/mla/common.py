825a7a46acc4cef0b9742253b273a11c39d138aa
Xinyu Chen <xinyu1.chen@intel.com>
2025-10-20 10:38:15 +0800

    [DeepSeek] reduce DMA transpose
    Signed-off-by: Xinyu Chen <xinyu1.chen@intel.com>

--- vllm/v1/attention/backends/mla/common.py
+++ vllm/v1/attention/backends/mla/common.py
@@ -1138,9 +1138,9 @@ class MLACommonBaseImpl(MLAAttentionImpl[A], Generic[A]):
                 )
         else:
             # Convert from (L, N, V) to (N, L, V)
-            self.W_UV = W_UV.transpose(0, 1)
+            self.W_UV = W_UV.transpose(0, 1).contiguous()
             # Convert from (L, N, P) to (N, P, L)
-            self.W_UK_T = W_UK.permute(1, 2, 0)
+            self.W_UK_T = W_UK.permute(1, 2, 0).contiguous()
 
     def _v_up_proj(self, x: torch.Tensor, out: torch.Tensor):
         # Convert from (B, N, L) to (N, B, L)
